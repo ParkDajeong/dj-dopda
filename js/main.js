9982bb30ef7fffcdd56aa25e0b349d5747aaecf4
ParkDajeong <sobeast980@gmail.com>
2021-05-17 23:00:42 +0900

    visual 반응형 및 동작 구현

--- js/main.js
+++ js/main.js
@@ -1,20 +1,41 @@
-$(window).resize(function() {
-  const window_w = $(window).width();
-
-  if(window_w > 1100) {
-    if($(".mo-nav").hasClass("on")) {
-      $(".gnb__btn-menu").removeClass("on");
-      $(".mo-nav").removeClass("on");
-      $(".gnb__bg").removeClass("on");
-    }
-  }
-})
-
-// mobile nav
 $(function() {
-  $(".gnb__btn-menu, .gnb__bg").on("click", function() {
-    $(".gnb__btn-menu").toggleClass("on");
-    $(".mo-nav").toggleClass("on");
-    $(".gnb__bg").toggleClass("on");
+  $(window).resize(function() {
+    const window_w = $(window).width();
+  
+    if(window_w > 1100) {
+      if($(".mo-nav").hasClass("on")) {
+        $(".gnb__btn-menu").removeClass("on");
+        $(".mo-nav").removeClass("on");
+        $(".gnb__bg").removeClass("on");
+      }
+    }
+  })
+  
+  // mobile nav
+  $(function() {
+    $(".gnb__btn-menu, .gnb__bg").on("click", function() {
+      $(".gnb__btn-menu").toggleClass("on");
+      $(".mo-nav").toggleClass("on");
+      $(".gnb__bg").toggleClass("on");
+    });
+  });
+  
+  // visual slide
+  $(function() {
+    $(".visual .slide").slick({
+      autoplay: true,
+      autoplaySpeed: 5000,
+      arrows: false,
+      dots: true,
+      fade: true,
+      pauseOnHover: false,
+      pauseOnFocus: false,
+    });
+  });
+  
+  // visual slide page
+  $(".visual .slide").on("beforeChange", function(_event, _slick, _current, next) {
+    $(".slide-page__item").removeClass("on");
+    $(".slide-page__item").eq(next).addClass("on");
   });
 });
